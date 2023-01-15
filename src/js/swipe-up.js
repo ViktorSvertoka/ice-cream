c4d5bc8a6d8fd8291a648001304d644542ebaad4
Viktor Svertoka <115661003+ViktorSvertoka@users.noreply.github.com>
2023-01-15 04:23:56 +0200

    update

--- src/js/swipe-up.js
+++ src/js/swipe-up.js
@@ -1,53 +1,27 @@
-function hasScrollBehavior() {
-  return 'scrollBehavior' in document.documentElement.style;
+document.addEventListener('scroll', handleScroll);
+// get a reference to our predefined button
+var scrollToTopBtn = document.querySelector('.scrollToTopBtn');
+
+function handleScroll() {
+  var scrollableHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var GOLDEN_RATIO = 0.2;
+
+  if (document.documentElement.scrollTop / scrollableHeight > GOLDEN_RATIO) {
+    //show button
+    scrollToTopBtn.style.display = 'block';
+  } else {
+    //hide button
+    scrollToTopBtn.style.display = 'none';
+  }
 }
 
-function smoothScroll() {
-  var currentY = window.scrollY;
-  var int = setInterval(function () {
-    window.scrollTo(0, currentY);
-
-    if (currentY > 500) {
-      currentY -= 70;
-    } else if (currentY > 100) {
-      currentY -= 50;
-    } else {
-      currentY -= 10;
-    }
-
-    if (currentY <= 0) clearInterval(int);
-  }, 1000 / 2); // 60fps
-}
+scrollToTopBtn.addEventListener('click', scrollToTop);
 
 function scrollToTop() {
-  if (hasScrollBehavior()) {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  } else {
-    smoothScroll();
-  }
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
 }
-
-function toggleScrollUpButton() {
-  var y = window.scrollY;
-  var e = document.getElementById('scroll-to-top');
-  if (y >= 200) {
-    e.style.transform = 'translateY(-30%)';
-    e.style.opacity = 1;
-  } else {
-    e.style.opacity = 0;
-    e.style.transform = 'translateY(30%)';
-  }
-}
-
-document.addEventListener(
-  'DOMContentLoaded',
-  function () {
-    document.removeEventListener('DOMContentLoaded', arguments.callee, false);
-
-    window.addEventListener('scroll', toggleScrollUpButton);
-
-    var e = document.getElementById('scroll-to-top');
-    e.addEventListener('click', scrollToTop, false);
-  },
-  false
-);
