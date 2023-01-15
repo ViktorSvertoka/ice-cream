754ce5cc149c5e5f848412a3c2acb35e345c4e31
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-01-15 01:24:54 +0100

    add products-modal.js set up btn:disabledjs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './js/gallery';
 import './js/modal';
 import './js/slider';
 import './js/franchise';
-import './js/up';
 import './js/header-modal';
 import './js/swipe-up';
+import './js/products-modal';
