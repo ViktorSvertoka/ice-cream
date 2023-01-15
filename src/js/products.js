c7536efedfc10a103c285ce33a6868395e9c4ddf
Samus Dimitriy <sax.samusdimitriy@gmail.com>
2023-01-16 00:36:46 +0100

    add some js for cow

--- src/js/products.js
+++ src/js/products.js
@@ -1,5 +1,6 @@
 let wh = window.innerHeight;
 let tolCard = wh * 0.7; //  scroll tolerance - eg. 70% from page top
+let tolAbout = wh * 0.2; //  scroll tolerance - eg. 10% from page top
 
 const inViewport = (elem, pix, cls) => {
   if (document.getElementsByClassName(elem).length > 0) {
@@ -19,6 +20,7 @@ const inViewport = (elem, pix, cls) => {
   }
 };
 
+inViewport('about__img', tolAbout, 'in-viewport');
 inViewport('products__item--green', tolCard, 'in-viewport');
 inViewport('products__item--coffee', tolCard, 'in-viewport');
 inViewport('products__item--milkshakes', tolCard, 'in-viewport');
